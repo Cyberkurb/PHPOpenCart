8fff1d403a95dd5aedf2a346dbbb9633feb2ffcd
Justin Lonsway <justin.lonsway@hotmail.com>
2021-08-27 13:09:45 -0700

    updates

--- affirm/_creds.php
+++
@@ -1,7 +0,0 @@
-<?php
-$sandbox_public_key = "YOUR_SANDBOX_PUBLIC_API_KEY";
-$sandbox_private_key = "YOUR_SANDBOX_PRIVATE_API_KEY";
-
-$live_public_key = "YOUR_LIVE_PUBLIC_API_KEY";
-$live_private_key = "YOUR_LIVE_PRIVATE_API_KEY";
-?>
--- affirm/creds.php
+++
@@ -1,7 +0,0 @@
-<?php
-$sandbox_public_key = "PIRVRINK36KYQ6XX";
-$sandbox_private_key = "zOagNqidUaNmGOWrdHjWoQHtiX9YQ4Jj";
-
-$live_public_key = "PIRVRINK36KYQ6XX";
-$live_private_key = "zOagNqidUaNmGOWrdHjWoQHtiX9YQ4Jj";
-?>
