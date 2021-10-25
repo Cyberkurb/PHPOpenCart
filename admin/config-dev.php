239601d09ef8b1443254fb80f72cf9fe1952a145
Justin Lonsway <cyberkurb@hotmail.com>
2021-10-25 14:45:28 -0700

    Update config-dev.php

--- admin/config-dev.php
+++ admin/config-dev.php
@@ -31,11 +31,11 @@ define('CACHE_PREFIX', 'oc_test');
 
 // DB
     define('DB_DRIVER', 'mysqli');
-    define('DB_HOSTNAME', 'dansons-cluster-1.cluster-cjocz1mn6ubo.us-west-2.rds.amazonaws.com');
+    define('DB_HOSTNAME', '');
     define('DB_HOSTNAME_READ', '');
-    define('DB_USERNAME', 'dansons_websites');
-    define('DB_PASSWORD', "y4P#zrvjW/q5-V%<");
-    define('DB_DATABASE', 'dbdansons');
+    define('DB_USERNAME', '');
+    define('DB_PASSWORD', "");
+    define('DB_DATABASE', '');
     define('DB_PORT', '3306');
     define('DB_PREFIX', 'oc_');
 
